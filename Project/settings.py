21a92ffdbf83a0dd224877c3aaab4f2e3d64c8d9
TraceWidler <twidler@unomaha.edu>
2023-06-13 08:31:32 -0500

    updating superuser

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5!960=z#niadyxi@n)z!_n%pst395nyfqgb(9em$427^xqva_g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pyhtonanywhere', 'localhost']
 
 
 # Application definition
