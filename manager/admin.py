f9ddf813e8a42e6d64e067d29c343e00dfec7324
CHOTIKA <climpanarak@gmail.com>
2023-06-28 18:57:42 -0500

    admin page roles

--- manager/admin.py
+++ manager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Property, Room
+from .models import Property, Room, UserProfile
 
 admin.site.register(Property)
+admin.site.register(UserProfile)
 admin.site.register(Room)
